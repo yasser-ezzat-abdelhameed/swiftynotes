908f3b21865f42a5ca27667df5a239acd3c92e8e
yasser-ezzat-abdelhameed <yasseraudio@gmail.com>
2019-07-06 12:04:44 +0200

    first commit

---
+++ SwiftyNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyNotes
+//
+//  Created by Yasser Ezzat on 7/6/19.
+//  Copyright © 2019 Yasser Ezzat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
